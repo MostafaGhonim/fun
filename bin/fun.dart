a662d03136db75443e29c1474dc64fbc19a307bd
mostafa ghonim <mostafasea7@gmail.com>
2022-08-14 10:32:02 +0200

    add new funcation "Hello"

--- bin/fun.dart
+++ bin/fun.dart
@@ -3,12 +3,16 @@ import 'package:fun/RandomNumberGuess.dart';
 
 
 main() {
-int askforrange = Howguess();
- int random = genratenumberforgusse(askforrange);
- int userin;
- do{
-  userin = askusergusse();
-  var result = compear(userin, random);
-  print(result);
- }while(userin != random);
+//   print("welcome to our game !!");
+//   var askfornumber= askRange();
+// var num = makeRandomNumber(askfornumber);
+// var user;
+// do {
+//   user = UserInput();
+//  var result = compervalue(user, num);
+//  print(result);
+// }while(user != num);
+var reslut = hello();
+print(reslut);
+
 }
--- lib/RandomNumberGuess.dart
+++ lib/RandomNumberGuess.dart
@@ -1,53 +1,39 @@
 import 'dart:io';
 import 'dart:math';
 
-gussnum(int inputnum){
-  Random computernum = Random();
-  print("hello, please enter number ");
-  var rand = computernum.nextInt(inputnum);
-  var gusse;
-  print("enter your guss");
-  do{
-    gusse = stdin.readLineSync();
-    gusse = int.parse(gusse);
-    if(gusse < rand){
-      print("more");
-    }
-    if(gusse > rand){
-      print("less");
-    }
-  }while(gusse != rand);
-  print("you got the number!! the number is $rand");
+int makeRandomNumber(int x){
+  Random rand = Random();
+  return rand.nextInt(x);
 }
-
-int genratenumberforgusse(int x){
-  Random randomnumber = Random();
-  return randomnumber.nextInt(x);
+int askRange(){
+  print("Enter the range:");
+  var userIn;
+  userIn = stdin.readLineSync();
+  return int.parse(userIn);
 }
-int Howguess(){
-  print("enter the range to guess :");
-  var HowMuch;
-  HowMuch = stdin.readLineSync();
-  HowMuch = int.parse(HowMuch);
-  return HowMuch;
+int UserInput(){
+  print("enter your guess");
+  var input;
+  input = stdin.readLineSync();
+  return int.parse(input);
 }
-int askusergusse(){
-  print("Enter your guess");
-  var guess;
-  guess= stdin.readLineSync();
-  return int.parse(guess);
-}
-
-String compear(int userin , int random){
-  if(userin < random){
-    return  "To high";
+String compervalue(int userval1 , int randval2){
+  if(userval1 > randval2){
+    return "To low";
+  }else if(userval1 < randval2){
+    return "To high";
+  }else{
+    return "you got the number is $randval2";
   }
-  else if(userin > random){
-    return  "To low";
-  }
-  else{
-    return "You are guessed it the number is $random";
-  }
-
 }
-
+List hello(){
+  print("Enter waht you want to print ? ");
+ var userin;
+ userin = stdin.readLineSync();
+ print("How often do you want to print it");
+ var userinNum;
+ userinNum = stdin.readLineSync();
+ userinNum = int.parse(userinNum);
+  final list = List.filled(userinNum, userin);
+  return list;
+}
